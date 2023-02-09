167c92c6dc235763d26a98f44439ed37f387d343
fabricio.oliveira <fabricio.oliveira>
2023-02-09 17:37:37 -0300

    Testes de unidade método Put

---
+++ ProductCrud.UnitTests/Application/Controllers/PutProductController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ProductApp.Application.Controllers;
+using ProductApp.Application.Interfaces.Services;
+using ProductApp.Application.Models;
+using ProductCrud.UnitTests.Fixtures;
+using Xunit;
+
+namespace ProductCrud.UnitTests.Application.Controllers
+{
+    public class PutProductController
+    {
+        [Fact]
+        public void Put_OnSuccess_ReturnsStatusCode204()
+        {
+            //Arrange
+            var dto = ProductFixtures.PutProductDTO();
+            var mockProductService = new Mock<IProductService>();
+            mockProductService
+                .Setup(service => service.Edit(dto))
+                .Returns(true);
+            var controller = new ProductsController(mockProductService.Object);
+            //Act
+            var result = (NoContentResult)controller.Put(dto.Id, dto);
+            //Assert
+            result.StatusCode.Should().Be(204);
+        }
+
+        [Fact]
+        public void Put_OnInvalidId_ReturnsStatusCode400()
+        {
+            //Arrange
+            var dto = ProductFixtures.PutProductDTO();
+            var mockProductService = new Mock<IProductService>();
+            mockProductService
+                .Setup(service => service.Edit(dto))
+                .Returns(true);
+            var controller = new ProductsController(mockProductService.Object);
+            //Act
+            var result = (BadRequestObjectResult)controller.Put(0, dto);
+            //Assert
+            result.StatusCode.Should().Be(400);
+        }
+
+        [Fact]
+        public void Put_OnInvalidValidateDate_ReturnsStatusCode400()
+        {
+            //Arrange
+            var dto = ProductFixtures.PutProductDTO();
+            dto.ValidateDate = dto.ValidateDate.Value.AddYears(-2);
+            var mockProductService = new Mock<IProductService>();
+            mockProductService
+                .Setup(service => service.Edit(dto))
+                .Returns(true);
+            var controller = new ProductsController(mockProductService.Object);
+            //Act
+            var result = (BadRequestObjectResult)controller.Put(dto.Id, dto);
+            //Assert
+            result.StatusCode.Should().Be(400);
+        }
+
+        [Fact]
+        public void Put_OnEditFailure_ReturnsStatusCode400()
+        {
+            //Arrange
+            var dto = ProductFixtures.PutProductDTO();
+            var mockProductService = new Mock<IProductService>();
+            mockProductService
+                .Setup(service => service.Edit(dto))
+                .Returns(false);
+            var controller = new ProductsController(mockProductService.Object);
+            //Act
+            var result = (BadRequestObjectResult)controller.Put(dto.Id, dto);
+            //Assert
+            result.StatusCode.Should().Be(400);
+        }
+    }
+}
---
+++ ProductCrud.UnitTests/Fixtures/ProductFixtures.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using ProductApp.Application.Models;
+using ProductApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCrud.UnitTests.Fixtures
+{
+    public class ProductFixtures
+    {
+
+        public static List<Product> GetTestProducts()  {
+            List<Product> items = new List<Product>();
+            for (int x = 1; x <= 50; x++) 
+            {
+                items.Add(new Product()
+                {
+                    Id= x,
+                    Description = ("Produto " + x),
+                    ValidateDate = DateTime.Now.AddYears(1),
+                    FabricationDate = DateTime.Now,
+                    SupplierId = x,
+                    IsActive = true,
+                    Supplier = new Supplier()
+                    {
+                        Description = ("Fornecedor " + x),
+                        Id = x,
+                        CNPJ = ("1231230001" + x),
+                        IsActive= true
+                    }
+                });
+            }
+            return items;
+        }
+
+        public static PutProductDTO PutProductDTO()
+        {
+            Product prod = GetTestProducts().First();
+            return new PutProductDTO()
+            {
+                Id = prod.Id,
+                Description = prod.Description,
+                ValidateDate = prod.ValidateDate,
+                SupplierId = prod.SupplierId,
+                FabricationDate = prod.FabricationDate
+            };
+        }
+    }
+}
